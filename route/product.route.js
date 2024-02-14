f3d4073c44ab086f6dacfe036a9de52557f2923e
satyam46020 <satyam46020@gmail.com>
2024-02-14 14:36:08 +0530

    pagination

--- route/product.route.js
+++ route/product.route.js
@@ -5,7 +5,7 @@ const Product = require('../model/Product.model');
 productrouter.get('/products', async (req, res) => {
   try {
     let query = {};
-
+    
     if (req.query.search) {
       query.name = { $regex: new RegExp(req.query.search, 'i') };
     }
@@ -18,20 +18,24 @@ productrouter.get('/products', async (req, res) => {
       query.category = req.query.category;
     }
 
+    const page = parseInt(req.query.page) || 1;  
+    const limit = parseInt(req.query.limit) || 10;  
+
+    const skip = (page - 1) * limit;
+
+    let productsQuery = Product.find(query);
+
     if (req.query.sort === 'asc') {
-      const products = await Product.find(query).sort({ price: 1 });
-      res.status(200).json(products);
-    }
-    else if (req.query.sort === 'desc') {
-      const products = await Product.find(query).sort({ price: -1 });
-      res.status(200).json(products);
+      productsQuery = productsQuery.sort({ price: 1 });
+    } else if (req.query.sort === 'desc') {
+      productsQuery = productsQuery.sort({ price: -1 });
     }
 
-    else {
-      const products = await Product.find(query);
-      res.status(200).json(products);
-    }
-  }  catch (error) {
+    const products = await productsQuery.skip(skip).limit(limit).exec();
+
+    res.status(200).json(products);
+  } 
+ catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Internal Server Error' });
   }
